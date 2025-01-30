1242845ee1257f33a4c4c25b5c6df58d6739769c
Magnet <phatthansn.charoenphol@gmail.com>
2025-01-30 10:01:14 +0700

    Add others

---
+++ src/component/contact.jsx
@@ -0,0 +1,66 @@
+function Contact(){
+    return(
+        <>
+            <div className="h-48">
+                <div className="flex justify-center mt-7 text-3xl font-bold">Contact</div>
+                <div className="flex flex-row justify-center m-6">
+
+                    <div className="flex space-x-4 justify-center items-center mx-4">
+                        <button className="bg-green-950 w-64 text-white border border-green-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                            <span className="bg-green-400 shadow-green-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                            <div className="flex flex-row items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                                <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1.003 1.003 0 011.11-.27 11.36 11.36 0 003.58.61 1 1 0 011 1v3.61a1 1 0 01-.92 1 19.34 19.34 0 01-8.4-2.7 19.518 19.518 0 01-6-6 19.34 19.34 0 01-2.7-8.4 1 1 0 011-1h3.6a1 1 0 011 1c0 1.24.2 2.43.61 3.58a1.003 1.003 0 01-.27 1.11l-2.2 2.2z"></path>
+                                </svg>
+
+                                <div className="m-2">Telephone: </div>
+                                <div>084-261-6102</div>
+                            </div>
+                        </button>
+                    </div>
+
+                    <div className="flex space-x-4 justify-center items-center mx-4">
+                        <button className="bg-gray-500 w-96 text-white border border-gray-700 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                            <span className="bg-black-400 shadow-black-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                            <div className="flex flex-row items-center justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                                <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 2v.51L12 11 4 6.51V6h16zm-1 12H5c-.55 0-1-.45-1-1v-9l8 5 8-5v9c0 .55-.45 1-1 1z"></path>
+                                </svg>
+                                <div className="m-2">Gmail: </div>
+                                <div>6733194421@student.chula.ac.th</div>
+                            </div>
+                        </button>
+                    </div>
+
+                    <div className="flex space-x-4 justify-center items-center mx-4">
+                        <button className="bg-gradient-to-r from-pink-700 via-purple-700 to-yellow-600 w-64 text-white border border-pink-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                            <span className="bg-pink-400 shadow-pink-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                            <div className="flex flex-row items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                                <path d="M7.75 2h8.5A5.25 5.25 0 0121.5 7.25v8.5A5.25 5.25 0 0116.25 21H7.75A5.25 5.25 0 012.5 15.75V7.25A5.25 5.25 0 017.75 2zm0 1.5a3.75 3.75 0 00-3.75 3.75v8.5a3.75 3.75 0 003.75 3.75h8.5a3.75 3.75 0 003.75-3.75V7.25a3.75 3.75 0 00-3.75-3.75H7.75zM12 6.75a5.25 5.25 0 110 10.5 5.25 5.25 0 010-10.5zm0 1.5a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5zm4.75-.75a1.25 1.25 0 110 2.5 1.25 1.25 0 010-2.5z"></path>
+                                </svg>
+
+                                <div className="m-2">Instagram: </div>
+                                <div>phatthanan_mg</div>
+                            </div>
+                        </button>
+                    </div>
+
+                    <div className="flex space-x-4 justify-center items-center mx-4">
+                        <button className="bg-blue-950 w-40 text-white border border-blue-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                            <span className="bg-blue-400 shadow-blue-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                            <div className="flex flex-row items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                                    <path d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.68-.22.68-.48v-1.69c-2.78.61-3.37-1.34-3.37-1.34-.46-1.17-1.11-1.48-1.11-1.48-.91-.62.07-.61.07-.61 1 .07 1.54 1.02 1.54 1.02.9 1.54 2.36 1.1 2.94.84.09-.65.35-1.1.63-1.35-2.22-.25-4.55-1.11-4.55-4.95 0-1.09.39-1.99 1.03-2.69-.1-.25-.45-1.27.1-2.65 0 0 .84-.27 2.75 1.03A9.568 9.568 0 0112 6.8c.85.004 1.7.11 2.5.32 1.91-1.3 2.75-1.03 2.75-1.03.55 1.38.2 2.4.1 2.65.64.7 1.03 1.6 1.03 2.69 0 3.85-2.33 4.7-4.55 4.95.36.31.68.93.68 1.88v2.79c0 .27.18.59.69.48C19.14 20.16 22 16.42 22 12c0-5.52-4.48-10-10-10z"></path>
+                                </svg>
+                                <div className="m-2">Github</div>
+                                <div><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"></path></svg></div>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Contact
